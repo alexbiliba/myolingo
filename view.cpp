f0fd45253c071086095ec02125115fa13f705e21
alexbiliba <alexbiliba@gmail.com>
2022-04-19 10:38:49 +0300

    View update: SetController

--- view.cpp
+++ view.cpp
@@ -1,1 +1,9 @@
 #include "view.h"
+
+#include <utility>
+
+void View::SetController(
+    const std::shared_ptr<AbstractController>& controller) {
+  assert(controller != nullptr);
+  controller_ = controller;
+}
--- view.h
+++ view.h
@@ -1,12 +1,19 @@
 #ifndef VIEW_H_
 #define VIEW_H_
 
+#include <memory>
 #include <QWidget>
+
+#include "abstract_controller.h"
 
 class View : public QWidget {
  public:
+  View() = default;
+
+  void SetController(const std::shared_ptr<AbstractController>& controller);
 
  private:
+  std::shared_ptr<AbstractController> controller_;
 };
 
 #endif  // VIEW_H_
