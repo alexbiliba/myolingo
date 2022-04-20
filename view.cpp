61388cdde1253b1db5b381953a6a383c3b5af83d
alexbiliba <alexbiliba@gmail.com>
2022-04-20 16:31:01 +0300

    View update: added main layout

--- view.cpp
+++ view.cpp
@@ -1,6 +1,13 @@
 #include "view.h"
 
 #include <utility>
+#include <iostream>
+
+View::View() : main_layout_(new QBoxLayout(
+                   QBoxLayout::Direction::TopToBottom, this)) {
+  std::cerr << "View was created\n";
+  main_layout_->addWidget(new QPushButton("example_button"));
+}
 
 void View::SetController(
     const std::shared_ptr<AbstractController>& controller) {
--- view.h
+++ view.h
@@ -2,17 +2,23 @@
 #define VIEW_H_
 
 #include <memory>
+#include <QBoxLayout>
+#include <QGroupBox>
+#include <QMainWindow>
+#include <QPushButton>
 #include <QWidget>
 
 #include "abstract_controller.h"
 
 class View : public QWidget {
+  Q_OBJECT
  public:
-  View() = default;
+  View();
 
   void SetController(const std::shared_ptr<AbstractController>& controller);
 
  private:
+  QBoxLayout* main_layout_;
   std::shared_ptr<AbstractController> controller_;
 };
 
